ba6677b84636e34fad3f881e3855d1ed642dd6fc
Vikraman S <112175036+VikramanSubramaniyan@users.noreply.github.com>
2023-05-07 10:33:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,8 @@ with imcol2:
     st.image('image/Logo_final.png')
 with imcol3:
     st.write("")
+st.markdown("<p style='text-align: center; color: black; font-size:20px;'><span style='font-weight: bold'>Problem Statement: </span>To classify whether GPT or Human-generated the given content.</p>", unsafe_allow_html=True)
+st.markdown("<hr style=height:2.5px;background-color:gray>",unsafe_allow_html=True)
 #---------Side bar-------#
 
 with st.sidebar:
